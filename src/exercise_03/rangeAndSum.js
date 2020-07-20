e6bcef96e846b5f2c09f5be534fc787a4ac7206b
yi dai <daiyi@yideMacBook-Pro.local>
2020-07-20 09:37:10 +0800

    [yi-dai]:range array and calculate  sum of array.

--- src/exercise_03/rangeAndSum.js
+++ src/exercise_03/rangeAndSum.js
@@ -6,8 +6,17 @@ export function range(start, end) {
   //
   // * Please implement this function and pass all the tests in range_and_array_spec.js.
   // * Please do NOT modify the signature of the function.
-
-  throw new Error('Please delete this line and implement the function');
+  const array = [];
+  if (start <= end) {
+    for (let i = start; i < end; i += 1) {
+      array.push(i);
+    }
+  } else {
+    for (let i = start; i > end; i -= 1) {
+      array.push(i);
+    }
+  }
+  return array;
 }
 
 export function sum(...numbers) {
@@ -18,5 +27,14 @@ export function sum(...numbers) {
   // * Please implement this function and pass all the tests in range_and_array_spec.js.
   // * Please do NOT modify the signature of the function.
 
-  throw new Error('Please delete this line and implement the function');
+  let result = 0;
+  numbers.map(Number);
+  if (numbers.length !== 0 && numbers.length !== 1) {
+    for (let i = 0; i < numbers.length; i += 1) {
+      result += numbers[i];
+    }
+  } else if (numbers.length === 1) {
+    result += numbers[0];
+  }
+  return result;
 }
