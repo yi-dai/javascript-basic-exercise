133a6b9667763b92a7d8725e7e0160a092eefe75
yi dai <daiyi@yideMacBook-Pro.local>
2020-07-29 15:39:37 +0800

    [yi-dai] edit ex02.

--- src/exercise_02/characterCounter.js
+++ src/exercise_02/characterCounter.js
@@ -7,21 +7,12 @@ export default function countCharacter(string, prediction) {
   // * Please do NOT modify the signature of the function.
 
   let result;
-  let a1;
-  let index;
-  let target;
   if (string === null || string === undefined) {
     result = 0;
   } else if (prediction) {
-    a1 = prediction.toString();
-    index = a1.indexOf('\'');
-    target = a1[index + 1];
-    result = target.toString();
-    a1 = string.toString();
-    index = a1.split(result);
-    result = 1;
-    for (let i = 0; i < index.length; i += 1) {
-      if (index[i].length === 0) {
+    result = 0;
+    for (let i = 0; i < string.length; i += 1) {
+      if (prediction(string[i])) {
         result += 1;
       }
     }
